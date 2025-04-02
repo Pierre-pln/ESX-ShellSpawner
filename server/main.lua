e6b750885b47ca8b4cba35ea899fdfbad8ede965
GR-Scripts <155775715+GR-Scripts@users.noreply.github.com>
2025-04-02 19:36:48 +0200

    GR-ShellSpawner

---
+++ server/main.lua
@@ -0,0 +1,14 @@
+QBCore = exports['qb-core']:GetCoreObject()
+
+AddEventHandler("onResourceStart", function(resourceName)
+    if GetCurrentResourceName() == resourceName then
+        Wait(1000)
+        TriggerClientEvent("gr-shellspawner:spawnShells", -1, Config.Shells)
+    end
+end)
+
+AddEventHandler("onResourceStop", function(resourceName)
+    if GetCurrentResourceName() == resourceName then
+        TriggerClientEvent("gr-shellspawner:clearShells", -1)
+    end
+end)
