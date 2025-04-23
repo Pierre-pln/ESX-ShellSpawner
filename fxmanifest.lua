4e05c192f991b92928455c204b7a182c55ddd835
Pierre-pln <pierre.poulain.levrai@gmail.com>
2025-04-23 10:23:56 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,9 +1,9 @@
 fx_version 'cerulean'
 game 'gta5'
 
-author 'GR-Scripts'
-description 'Shell Spawner for QBCore'
-version '1.0.0'
+author 'Pierre <3'
+description 'Shell Spawner for ESX'
+version '0.1.0'
 
 shared_script 'config.lua'
 client_script 'client/main.lua'
