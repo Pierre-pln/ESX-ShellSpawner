0bf1aee3f0ec029732fca4b923e628c788d20a6c
Pierre-pln <pierre.poulain.levrai@gmail.com>
2025-04-23 10:26:10 +0200

    Update main.lua
    Switch from qb to esx

--- server/main.lua
+++ server/main.lua
@@ -1,14 +1,14 @@
-QBCore = exports['qb-core']:GetCoreObject()
+ESX = exports['es_extended']:getSharedObject()
 
 AddEventHandler("onResourceStart", function(resourceName)
     if GetCurrentResourceName() == resourceName then
         Wait(1000)
-        TriggerClientEvent("gr-shellspawner:spawnShells", -1, Config.Shells)
+        TriggerClientEvent("esx-shellspawner:spawnShells", -1, Config.Shells)
     end
 end)
 
 AddEventHandler("onResourceStop", function(resourceName)
     if GetCurrentResourceName() == resourceName then
-        TriggerClientEvent("gr-shellspawner:clearShells", -1)
+        TriggerClientEvent("esx-shellspawner:clearShells", -1)
     end
 end)
